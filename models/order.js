6ff670768669c1555f9a389a549c605d59e9361b
Aryan <aryan.khubchandani@gmail.com>
2023-03-31 00:11:34 +0530

    feat: add error handling

--- models/order.js
+++ models/order.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-  userId: { type: String, required: true },
-  cafeId: { type: String, required: true },
-  orderItems: { type: Map, of: Number, required: true },
-  total: { type: Number, required: true },
+  userId: { type: String, required: [true, "User ID is required"] },
+  cafeId: { type: String, required: [true, "Cafe ID is required"] },
+  orderItems: {
+    type: Map,
+    of: Number,
+    required: [true, "Order items are required"],
+  },
+  total: { type: Number, required: [true, "Total amoung is required"] },
+  readyBy: { type: Date },
 });
 
 OrderSchema.set("timestamps", true);
